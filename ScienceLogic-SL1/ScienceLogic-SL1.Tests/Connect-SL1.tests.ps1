41739e6a7ff90475747eaa0822ab8fb8b6b8e926
tom.robijns <34503431+trir262@users.noreply.github.com>
2018-06-25 09:34:06 +0200

    Separate tests as a file per function

---
+++ ScienceLogic-SL1/ScienceLogic-SL1.Tests/Connect-SL1.tests.ps1
@@ -0,0 +1,34 @@
+﻿Import-Module "$($psscriptroot)\..\ScienceLogic-SL1\ScienceLogic-SL1.psm1" -Force
+$SL1Cred = Import-Clixml -Path "$($MyInvocation.MyCommand.Path)\..\..\..\PrivateData\admin.xml"
+$SL1URI = (Get-Content "$($MyInvocation.MyCommand.Path)\..\..\..\PrivateData\URI.json" | ConvertFrom-Json).URI
+
+$Resources = ConvertFrom-Json "{ 'v1': {'SuccessMock' : 'Connected', 'SuccessCode' : '200' , 'URI': '$($SL1URI)' } }"
+
+Describe 'Connect-SL1' {
+	Context 'Logging in' {
+		It 'Ensures that Resources are loaded' {
+			$Resources | Should Not BeNullOrEmpty
+		}
+
+		It 'Validates Credentials' {
+			Mock Invoke-WebRequest -Verifiable -MockWith {
+				return @{
+					Content='Connected'
+					StatusCode=[system.net.httpstatuscode]::OK
+				}
+			} -ParameterFilter {
+				$uri -match $Resources.v1.URI
+			} -ModuleName ScienceLogic-SL1
+			$SL1Connection = Connect-SL1 -URI $SL1URI -Credential $SL1Cred -Passthru
+			$SL1Connection.IsConnected | Should Be $true
+
+			Assert-VerifiableMock
+
+		}
+
+		It 'Validates a successful connection' {
+			$Return = Connect-SL1 -uri $SL1URI -Credential $SL1Cred -Passthru
+			$Return.IsConnected | Should be $true
+		}
+	}
+}
--- ScienceLogic-SL1/ScienceLogic-SL1.Tests/Get-SL1Device.tests.ps1
+++ ScienceLogic-SL1/ScienceLogic-SL1.Tests/Get-SL1Device.tests.ps1
@@ -4,35 +4,6 @@ $SL1URI = (Get-Content "$($MyInvocation.MyCommand.Path)\..\..\..\PrivateData\URI
 
 $Resources = ConvertFrom-Json "{ 'v1': {'SuccessMock' : 'Connected', 'SuccessCode' : '200' , 'URI': '$($SL1URI)' } }"
 
-Describe 'Connect-SL1' {
-	Context 'Logging in' {
-		It 'Ensures that Resources are loaded' {
-			$Resources | Should Not BeNullOrEmpty
-		}
-
-		It 'Validates Credentials' {
-			Mock Invoke-WebRequest -Verifiable -MockWith {
-				return @{
-					Content='Connected'
-					StatusCode=[system.net.httpstatuscode]::OK
-				}
-			} -ParameterFilter {
-				$uri -match $Resources.v1.URI
-			} -ModuleName ScienceLogic-SL1
-			$SL1Connection = Connect-SL1 -URI $SL1URI -Credential $SL1Cred -Passthru
-			$SL1Connection.IsConnected | Should Be $true
-
-			Assert-VerifiableMock
-
-		}
-
-		It 'Validates a successful connection' {
-			$Return = Connect-SL1 -uri $SL1URI -Credential $SL1Cred -Passthru
-			$Return.IsConnected | Should be $true
-		}
-	}
-}
-
 Describe 'Get-SL1Device' {
 	Context 'Validate ParameterSetName ID' {
 		It 'Verifies a single device is returned' {
